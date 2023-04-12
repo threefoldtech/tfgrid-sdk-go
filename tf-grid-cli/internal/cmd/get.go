d5f9277d7e4f496fb946c341f6006c259b104f33
AbdelrahmanElawady <abdoelawady125@gmail.com>
2023-04-12 13:33:21 +0200

    Refactor get functions

--- tf-grid-cli/internal/cmd/get.go
+++ tf-grid-cli/internal/cmd/get.go
@@ -10,34 +10,24 @@ import (
 	"github.com/threefoldtech/tfgrid-sdk-go/grid3-go/workloads"
 )
 
+var (
+	vmType          = "vm"
+	gatewayNameType = "Gateway Name"
+	gatewayFQDNType = "Gateway Fqdn"
+	k8sType         = "kubernetes"
+	// networkType     = "network"
+)
+
 // GetVM gets a vm with its project name
 func GetVM(t deployer.TFPluginClient, name string) (workloads.Deployment, error) {
-	contracts, err := t.ContractsGetter.ListContractsOfProjectName(name)
+	nodeContractIDs, err := getContractsByTypeAndName(t, name, vmType, name)
 	if err != nil {
 		return workloads.Deployment{}, err
 	}
 	var nodeID uint32
-	var contractID uint64
-	for _, contract := range contracts.NodeContracts {
-		var deploymentData workloads.DeploymentData
-		err := json.Unmarshal([]byte(contract.DeploymentData), &deploymentData)
-		if err != nil {
-			return workloads.Deployment{}, err
-		}
-		if deploymentData.Type != "vm" || deploymentData.ProjectName != name {
-			continue
-		}
-		nodeID = contract.NodeID
-		contractID, err = strconv.ParseUint(contract.ContractID, 0, 64)
-		if err != nil {
-			return workloads.Deployment{}, err
-		}
-
-		t.State.CurrentNodeDeployments[nodeID] = []uint64{contractID}
-		break
-	}
-	if nodeID == 0 {
-		return workloads.Deployment{}, fmt.Errorf("no vm with name %s found", name)
+	for node, contractID := range nodeContractIDs {
+		t.State.CurrentNodeDeployments[node] = []uint64{contractID}
+		nodeID = node
 	}
 
 	return t.State.LoadDeploymentFromGrid(nodeID, name)
@@ -45,93 +35,76 @@ func GetVM(t deployer.TFPluginClient, name string) (workloads.Deployment, error)
 
 // GetK8sCluster gets a kubernetes cluster with its project name
 func GetK8sCluster(t deployer.TFPluginClient, name string) (workloads.K8sCluster, error) {
-	contracts, err := t.ContractsGetter.ListContractsOfProjectName(name)
+	nodeContractIDs, err := getContractsByTypeAndName(t, name, k8sType, name)
 	if err != nil {
 		return workloads.K8sCluster{}, err
 	}
 	var nodeIDs []uint32
-	for _, contract := range contracts.NodeContracts {
-		var deploymentData workloads.DeploymentData
-		err := json.Unmarshal([]byte(contract.DeploymentData), &deploymentData)
-		if err != nil {
-			return workloads.K8sCluster{}, err
-		}
-		if deploymentData.Type != "kubernetes" || deploymentData.Name != name {
-			continue
-		}
-		nodeIDs = append(nodeIDs, contract.NodeID)
-		contractID, err := strconv.ParseUint(contract.ContractID, 0, 64)
-		if err != nil {
-			return workloads.K8sCluster{}, err
-		}
-		t.State.CurrentNodeDeployments[contract.NodeID] = append(t.State.CurrentNodeDeployments[contract.NodeID], contractID)
+	for node, contractID := range nodeContractIDs {
+		t.State.CurrentNodeDeployments[node] = []uint64{contractID}
+		nodeIDs = append(nodeIDs, node)
 	}
-	if nodeIDs == nil {
-		return workloads.K8sCluster{}, fmt.Errorf("no k8s cluster with name %s found", name)
-	}
+
 	return t.State.LoadK8sFromGrid(nodeIDs, name)
 }
 
 // GetGatewayName gets a gateway name with its project name
 func GetGatewayName(t deployer.TFPluginClient, name string) (workloads.GatewayNameProxy, error) {
-	contracts, err := t.ContractsGetter.ListContractsOfProjectName(name)
+	nodeContractIDs, err := getContractsByTypeAndName(t, name, gatewayNameType, name)
 	if err != nil {
 		return workloads.GatewayNameProxy{}, err
 	}
 	var nodeID uint32
-	var contractID uint64
-	for _, contract := range contracts.NodeContracts {
-		var deploymentData workloads.DeploymentData
-		err := json.Unmarshal([]byte(contract.DeploymentData), &deploymentData)
-		if err != nil {
-			return workloads.GatewayNameProxy{}, err
-		}
-		if deploymentData.Type != "Gateway Name" || deploymentData.Name != name {
-			continue
-		}
-		nodeID = contract.NodeID
-		contractID, err = strconv.ParseUint(contract.ContractID, 0, 64)
-		if err != nil {
-			return workloads.GatewayNameProxy{}, err
-		}
+	for node, contractID := range nodeContractIDs {
+		t.State.CurrentNodeDeployments[node] = []uint64{contractID}
+		nodeID = node
+	}
 
-		t.State.CurrentNodeDeployments[nodeID] = []uint64{contractID}
-		break
-	}
-	if nodeID == 0 {
-		return workloads.GatewayNameProxy{}, fmt.Errorf("no gateway name with name %s found", name)
-	}
 	return t.State.LoadGatewayNameFromGrid(nodeID, name, name)
 }
 
 // GetGatewayFQDN gets a gateway fqdn with its project name
 func GetGatewayFQDN(t deployer.TFPluginClient, name string) (workloads.GatewayFQDNProxy, error) {
-	contracts, err := t.ContractsGetter.ListContractsOfProjectName(name)
+	nodeContractIDs, err := getContractsByTypeAndName(t, name, gatewayFQDNType, name)
 	if err != nil {
 		return workloads.GatewayFQDNProxy{}, err
 	}
 	var nodeID uint32
-	var contractID uint64
+	for node, contractID := range nodeContractIDs {
+		t.State.CurrentNodeDeployments[node] = []uint64{contractID}
+		nodeID = node
+	}
+
+	return t.State.LoadGatewayFQDNFromGrid(nodeID, name, name)
+}
+
+func getContractsByTypeAndName(t deployer.TFPluginClient, projectName, deploymentType, deploymentName string) (map[uint32]uint64, error) {
+	contracts, err := t.ContractsGetter.ListContractsOfProjectName(projectName)
+	if err != nil {
+		return map[uint32]uint64{}, err
+	}
+	nodeContractIDs := make(map[uint32]uint64)
 	for _, contract := range contracts.NodeContracts {
 		var deploymentData workloads.DeploymentData
 		err := json.Unmarshal([]byte(contract.DeploymentData), &deploymentData)
 		if err != nil {
-			return workloads.GatewayFQDNProxy{}, err
+			return map[uint32]uint64{}, err
 		}
-		if deploymentData.Type != "Gateway Fqdn" || deploymentData.Name != name {
+		if deploymentData.Type != deploymentType || deploymentData.Name != deploymentName {
 			continue
 		}
-		nodeID = contract.NodeID
-		contractID, err = strconv.ParseUint(contract.ContractID, 0, 64)
+		contractID, err := strconv.ParseUint(contract.ContractID, 0, 64)
 		if err != nil {
-			return workloads.GatewayFQDNProxy{}, err
+			return map[uint32]uint64{}, err
 		}
-
-		t.State.CurrentNodeDeployments[nodeID] = []uint64{contractID}
-		break
+		nodeContractIDs[contract.NodeID] = contractID
+		// only k8s and network have multiple contracts
+		if deploymentType == vmType || deploymentType == gatewayFQDNType || deploymentType == gatewayNameType {
+			break
+		}
 	}
-	if nodeID == 0 {
-		return workloads.GatewayFQDNProxy{}, fmt.Errorf("no gateway fqdn with name %s found", name)
+	if len(nodeContractIDs) == 0 {
+		return map[uint32]uint64{}, fmt.Errorf("no %s with name %s found", deploymentType, deploymentName)
 	}
-	return t.State.LoadGatewayFQDNFromGrid(nodeID, name, name)
+	return nodeContractIDs, nil
 }
