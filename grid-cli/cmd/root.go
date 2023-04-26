c89050cad23b46368852448454920f0cc8c22021
AbdelrahmanElawady <abdoelawady125@gmail.com>
2023-04-26 16:27:49 +0200

    Update root command to tf-grid-cli

--- grid-cli/cmd/root.go
+++ grid-cli/cmd/root.go
@@ -11,7 +11,7 @@ import (
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
-	Use:   "tf-grid",
+	Use:   "tf-grid-cli",
 	Short: "A cli for interacting with Threefold Grid",
 }
 
