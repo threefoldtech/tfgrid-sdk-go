b41d6739e6da7cb8b8cf4a53a260a7b4626c658e
rawdaGastan <rody.rawda.rr@gmail.com>
2023-06-22 13:45:01 +0300

    add a gpu test

--- grid-client/integration_tests/vm_gpu_test.go
+++ grid-client/integration_tests/vm_gpu_test.go
@@ -8,7 +8,9 @@ import (
 	"time"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/threefoldtech/tfgrid-sdk-go/grid-client/deployer"
 	"github.com/threefoldtech/tfgrid-sdk-go/grid-client/workloads"
+	"github.com/threefoldtech/tfgrid-sdk-go/grid-proxy/pkg/types"
 	"github.com/threefoldtech/zos/client"
 	"github.com/threefoldtech/zos/pkg/gridtypes"
 	"github.com/threefoldtech/zos/pkg/gridtypes/zos"
@@ -26,14 +28,27 @@ func TestVMWithGPUDeployment(t *testing.T) {
 	tfPluginClient, err := setup()
 	assert.NoError(t, err)
 
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Minute)
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Minute)
 	defer cancel()
 
-	publicKey, _, err := GenerateSSHKeyPair()
+	publicKey, privateKey, err := GenerateSSHKeyPair()
 	assert.NoError(t, err)
 
-	// TODO: add a filtered node
-	nodeID := uint32(93)
+	twinID := uint64(tfPluginClient.TwinID)
+	nodeFilter := types.NodeFilter{
+		Status:   &statusUp,
+		FreeSRU:  convertGBToBytes(20),
+		FreeMRU:  convertGBToBytes(8),
+		RentedBy: &twinID,
+		HasGPU:   &trueVal,
+	}
+
+	nodes, err := deployer.FilterNodes(ctx, tfPluginClient, nodeFilter)
+	if err != nil {
+		t.Skip("no available nodes found")
+	}
+	nodeID := uint32(nodes[0].NodeID)
+
 	nodeClient, err := tfPluginClient.NcPool.GetNodeClient(tfPluginClient.SubstrateConn, nodeID)
 	assert.NoError(t, err)
 
@@ -51,18 +66,25 @@ func TestVMWithGPUDeployment(t *testing.T) {
 		AddWGAccess: false,
 	}
 
+	disk := workloads.Disk{
+		Name:   "gpuDisk",
+		SizeGB: 20,
+	}
+
 	vm := workloads.VM{
 		Name:       "gpu",
-		Flist:      "https://hub.grid.tf/tf-official-apps/threefoldtech-ubuntu-22.04.flist",
-		CPU:        2,
+		Flist:      "https://hub.grid.tf/tf-official-vms/ubuntu-22.04.flist",
+		CPU:        4,
 		Planetary:  true,
-		Memory:     1024,
+		Memory:     1024 * 8,
 		GPUs:       ConvertGPUsToStr(gpus),
-		Entrypoint: "/sbin/zinit init",
+		Entrypoint: "/init.sh",
 		EnvVars: map[string]string{
 			"SSH_KEY": publicKey,
 		},
-		IP:          "10.20.2.5",
+		Mounts: []workloads.Mount{
+			{DiskName: disk.Name, MountPoint: "/data"},
+		},
 		NetworkName: network.Name,
 	}
 
@@ -74,7 +96,7 @@ func TestVMWithGPUDeployment(t *testing.T) {
 		assert.NoError(t, err)
 	}()
 
-	dl := workloads.NewDeployment("gpu", nodeID, "", nil, network.Name, nil, nil, []workloads.VM{vm}, nil)
+	dl := workloads.NewDeployment("gpu", nodeID, "", nil, network.Name, []workloads.Disk{disk}, nil, []workloads.VM{vm}, nil)
 	err = tfPluginClient.DeploymentDeployer.Deploy(ctx, &dl)
 	assert.NoError(t, err)
 
@@ -83,6 +105,12 @@ func TestVMWithGPUDeployment(t *testing.T) {
 		assert.NoError(t, err)
 	}()
 
-	_, err = tfPluginClient.State.LoadVMFromGrid(nodeID, vm.Name, dl.Name)
+	vm, err = tfPluginClient.State.LoadVMFromGrid(nodeID, vm.Name, dl.Name)
 	assert.NoError(t, err)
+	assert.Equal(t, vm.GPUs, ConvertGPUsToStr(gpus))
+
+	time.Sleep(30 * time.Second)
+	output, err := RemoteRun("root", vm.YggIP, "lspci -v", privateKey)
+	assert.NoError(t, err)
+	assert.Contains(t, string(output), gpus[0].Vendor)
 }
