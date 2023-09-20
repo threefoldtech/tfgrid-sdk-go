3c7e1f58c50e67947312d8c587e518172aec5762
Omar Abdulaziz <omarabdul3ziz@gmail.com>
2023-09-20 14:46:23 +0300

    use subquery with inner join with the node_gpu table

--- grid-proxy/internal/explorer/db/postgres.go
+++ grid-proxy/internal/explorer/db/postgres.go
@@ -377,7 +377,7 @@ func (d *PostgresDatabase) nodeTableQuery() *gorm.DB {
 	return d.gormDB.
 		Table("node").
 		Select(
-			"DISTINCT node.id",
+			"node.id",
 			"node.node_id",
 			"node.farm_id",
 			"node.twin_id",
@@ -429,9 +429,6 @@ func (d *PostgresDatabase) nodeTableQuery() *gorm.DB {
 		).
 		Joins(
 			"LEFT JOIN location ON node.location_id = location.id",
-		).
-		Joins(
-			"LEFT JOIN node_gpu on node_gpu.node_twin_id = node.twin_id",
 		)
 }
 
@@ -526,28 +523,44 @@ func (d *PostgresDatabase) GetNodes(filter types.NodeFilter, limit types.Limit) 
 		q = q.Where("node.certification ILIKE ?", *filter.CertificationType)
 	}
 
+	nodeGpuSubquery := d.gormDB.Table("node_gpu").
+		Select(`DISTINCT ON (node_twin_id) node_twin_id, 
+			id,
+			vendor,
+			device,
+			contract`,
+		)
+
 	if filter.HasGPU != nil {
-		q = q.Where("(COALESCE(node_gpu.id, '') != '') = ?", *filter.HasGPU)
+		nodeGpuSubquery = nodeGpuSubquery.Where("(COALESCE(node_gpu.id, '') != '') = ?", *filter.HasGPU)
 	}
 
 	if filter.GpuDeviceName != nil {
-		q = q.Where("COALESCE(node_gpu.device, '') ILIKE '%' || ? || '%'", *filter.GpuDeviceName)
+		nodeGpuSubquery = nodeGpuSubquery.Where("COALESCE(node_gpu.device, '') ILIKE '%' || ? || '%'", *filter.GpuDeviceName)
 	}
 
 	if filter.GpuVendorName != nil {
-		q = q.Where("COALESCE(node_gpu.vendor, '') ILIKE '%' || ? || '%'", *filter.GpuVendorName)
+		nodeGpuSubquery = nodeGpuSubquery.Where("COALESCE(node_gpu.vendor, '') ILIKE '%' || ? || '%'", *filter.GpuVendorName)
 	}
 
 	if filter.GpuVendorID != nil {
-		q = q.Where("COALESCE(node_gpu.id, '') ILIKE '%' || ? || '%'", *filter.GpuVendorID)
+		nodeGpuSubquery = nodeGpuSubquery.Where("COALESCE(node_gpu.id, '') ILIKE '%' || ? || '%'", *filter.GpuVendorID)
 	}
 
 	if filter.GpuDeviceID != nil {
-		q = q.Where("COALESCE(node_gpu.id, '') ILIKE '%' || ? || '%'", *filter.GpuDeviceID)
+		nodeGpuSubquery = nodeGpuSubquery.Where("COALESCE(node_gpu.id, '') ILIKE '%' || ? || '%'", *filter.GpuDeviceID)
 	}
 
 	if filter.GpuAvailable != nil {
-		q = q.Where("(COALESCE(node_gpu.contract, 0) = 0) = ?", *filter.GpuAvailable)
+		nodeGpuSubquery = nodeGpuSubquery.Where("(COALESCE(node_gpu.contract, 0) = 0) = ?", *filter.GpuAvailable)
+	}
+
+	if filter.HasGPU != nil || filter.GpuDeviceName != nil || filter.GpuVendorName != nil || filter.GpuVendorID != nil ||
+		filter.GpuDeviceID != nil || filter.GpuAvailable != nil {
+
+		q.Joins(
+			`INNER JOIN (?) AS gpu ON gpu.node_twin_id = node.twin_id`, nodeGpuSubquery,
+		)
 	}
 
 	var count int64
