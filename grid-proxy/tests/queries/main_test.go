8527128b1a1c7e003cd0f44d33cf3856a514255c
Eslam-Nawara <eslamnawara44@gmail.com>
2023-10-18 16:20:27 +0300

    revert changes in grid proxy

--- grid-proxy/tests/queries/main_test.go
+++ grid-proxy/tests/queries/main_test.go
@@ -46,7 +46,7 @@ func parseCmdline() {
 func TestMain(m *testing.M) {
 	parseCmdline()
 	if SEED != 0 {
-		rand.Seed(int64(SEED))
+		rand.New(rand.NewSource(int64(SEED)))
 	}
 
 	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
