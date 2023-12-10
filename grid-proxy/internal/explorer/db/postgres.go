4480a86d5257d7acc483968cd47a4610e32fdf01
Omar Abdulaziz <omarabdul3ziz@gmail.com>
2023-12-10 16:40:25 +0200

    wip: add startup materialized views

--- grid-proxy/internal/explorer/db/postgres.go
+++ grid-proxy/internal/explorer/db/postgres.go
@@ -31,8 +31,109 @@ var (
 	ErrContractNotFound = errors.New("contract not found")
 )
 
+var views = []string{
+	// Node view needed for join on farm
+	`
+	CREATE MATERIALIZED VIEW node_materialized_view AS
+	SELECT
+		node.node_id,
+		node.twin_id,
+		node.farm_id,
+		node.power,
+		node.updated_at,
+		node.certification,
+		node.country,
+		nodes_resources_view.free_mru,
+		nodes_resources_view.free_hru,
+		nodes_resources_view.free_sru,
+		COALESCE(rent_contract.contract_id, 0) as rent_contract_id,
+		COALESCE(rent_contract.twin_id, 0) as renter,
+		COALESCE(node_gpu.id, '') as gpu_id
+	FROM
+		node
+	LEFT JOIN nodes_resources_view ON node.node_id = nodes_resources_view.node_id
+	LEFT JOIN rent_contract ON node.node_id = rent_contract.node_id AND rent_contract.state IN ('Created', 'GracePeriod')
+	LEFT JOIN node_gpu ON node.twin_id = node_gpu.node_twin_id;
+
+	-- define a refresher function
+	CREATE OR REPLACE FUNCTION refresh_node_materialized_view()
+		RETURNS TRIGGER AS $$
+	BEGIN
+		REFRESH MATERIALIZED VIEW node_materialized_view;
+		RETURN NULL;
+	END;
+	$$ LANGUAGE plpgsql;
+	
+	-- trigger the refresh on each node update
+	CREATE TRIGGER refresh_node_materialized_trigger
+	AFTER UPDATE ON node
+	FOR EACH ROW
+	EXECUTE FUNCTION refresh_node_materialized_view();
+
+	-- trigger a backup refresh each 6 hours
+	SELECT cron.schedule('0 */6 * * *', 'SELECT refresh_node_materialized_view()');
+	`,
+	// Farm view
+	`
+	CREATE MATERIALIZED VIEW farm_materialized_view AS
+	SELECT
+		farm.id,
+		farm.farm_id,
+		farm.name,
+		farm.twin_id,
+		farm.pricing_policy_id,
+		farm.certification,
+		farm.stellar_address,
+		farm.dedicated_farm as dedicated,
+		COALESCE(public_ips.public_ips, '[]') as public_ips,
+		bool_or(node_materialized_view.rent_contract_id != 0) as has_rent_contract
+	FROM
+		farm
+
+	LEFT JOIN node_materialized_view ON node_materialized_view.farm_id = farm.farm_id
+	LEFT JOIN country ON node_materialized_view.country = country.name
+
+	LEFT JOIN (
+		SELECT
+			p1.farm_id,
+			COUNT(p1.id) total_ips,
+			COUNT(CASE WHEN p2.contract_id = 0 THEN 1 END) free_ips
+		FROM
+			public_ip p1
+		LEFT JOIN public_ip p2 ON p1.id = p2.id
+		GROUP BY p1.farm_id
+	) public_ip_count on public_ip_count.farm_id = farm.id
+
+	LEFT JOIN (
+		SELECT 
+			farm_id,
+			jsonb_agg(jsonb_build_object('id', id, 'ip', ip, 'contract_id', contract_id, 'gateway', gateway)) as public_ips
+		FROM
+			public_ip
+		GROUP BY farm_id
+	) public_ips on public_ips.farm_id = farm.id
+
+
+	GROUP BY
+		farm.id,
+		farm.farm_id,
+		farm.name,
+		farm.twin_id,
+		farm.pricing_policy_id,
+		farm.certification,
+		farm.stellar_address,
+		farm.dedicated_farm,
+		COALESCE(public_ips.public_ips, '[]');
+	`,
+}
+
 const (
 	setupPostgresql = `
+	CREATE INDEX IF NOT EXISTS idx_node_id ON public.node(node_id);
+	CREATE INDEX IF NOT EXISTS idx_twin_id ON public.twin(twin_id);
+	CREATE INDEX IF NOT EXISTS idx_farm_id ON public.farm(farm_id);
+	CREATE INDEX IF NOT EXISTS idx_contract_id ON public.node_contract(contract_id);
+
 	CREATE OR REPLACE VIEW nodes_resources_view AS SELECT
 		node.node_id,
 		COALESCE(sum(contract_resources.cru), 0) as used_cru,
@@ -121,7 +222,9 @@ func NewPostgresDatabase(host string, port int, user, password, dbname string, m
 	connString := fmt.Sprintf("host=%s port=%d user=%s "+
 		"password=%s dbname=%s sslmode=disable",
 		host, port, user, password, dbname)
-	gormDB, err := gorm.Open(postgres.Open(connString), &gorm.Config{})
+	gormDB, err := gorm.Open(postgres.Open(connString), &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Info),
+	})
 	if err != nil {
 		return nil, errors.Wrap(err, "failed to create orm wrapper around db")
 	}
@@ -155,8 +258,12 @@ func (d *PostgresDatabase) Close() error {
 }
 
 func (d *PostgresDatabase) initialize() error {
-	res := d.gormDB.Exec(setupPostgresql)
-	return res.Error
+	err := d.gormDB.Exec(setupPostgresql).Error
+	for _, view := range views {
+		err = d.gormDB.Exec(view).Error
+	}
+
+	return err
 }
 
 // GetStats returns aggregate info about the grid
@@ -382,13 +489,13 @@ func (d *PostgresDatabase) farmTableQuery() *gorm.DB {
 			LEFT JOIN public_ip p2 ON p1.id = p2.id
 			GROUP BY p1.farm_id
 		) public_ip_count on public_ip_count.farm_id = farm.id`).
-		Joins(`left join (
-			select 
-				farm_id,
-				jsonb_agg(jsonb_build_object('id', id, 'ip', ip, 'contract_id', contract_id, 'gateway', gateway)) as public_ips
-			from public_ip
-			GROUP BY farm_id
-		) public_ips on public_ips.farm_id = farm.id`).
+		// Joins(`left join (
+		// 	select
+		// 		farm_id,
+		// 		jsonb_agg(jsonb_build_object('id', id, 'ip', ip, 'contract_id', contract_id, 'gateway', gateway)) as public_ips
+		// 	from public_ip
+		// 	GROUP BY farm_id
+		// ) public_ips on public_ips.farm_id = farm.id`).
 		Joins(
 			"LEFT JOIN country ON node.country = country.name",
 		).
