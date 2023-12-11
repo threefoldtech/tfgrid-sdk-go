dac0346147ecce0b1adc27fdf179d4e66c702a19
AbdelrahmanElawady <abdoelawady125@gmail.com>
2023-12-11 14:44:21 +0200

    Add cancel contracts cmd

---
+++ grid-cli/cmd/cancel_contracts.go
@@ -0,0 +1,44 @@
+package cmd
+
+import (
+	"strconv"
+
+	"github.com/rs/zerolog/log"
+	"github.com/spf13/cobra"
+	"github.com/threefoldtech/tfgrid-sdk-go/grid-cli/internal/config"
+	"github.com/threefoldtech/tfgrid-sdk-go/grid-client/deployer"
+)
+
+// cancelContracts represents the cancel contracts command
+var cancelContracts = &cobra.Command{
+	Use:   "contracts",
+	Short: "Cancel twin contracts",
+	Run: func(cmd *cobra.Command, args []string) {
+		cfg, err := config.GetUserConfig()
+		if err != nil {
+			log.Fatal().Err(err).Send()
+		}
+		t, err := deployer.NewTFPluginClient(cfg.Mnemonics, "sr25519", cfg.Network, "", "", "", 100, false)
+		if err != nil {
+			log.Fatal().Err(err).Send()
+		}
+		var contracts []uint64
+		for _, arg := range args {
+			contract, err := strconv.Atoi(arg)
+			if err != nil {
+				log.Fatal().Err(err).Msgf("%s not a valid contract id", arg)
+			}
+			contracts = append(contracts, uint64(contract))
+		}
+		err = t.BatchCancelContract(contracts)
+		if err != nil {
+			log.Fatal().Err(err).Msg("failed to cancel contracts")
+		}
+		log.Info().Msg("contracts canceled successfully")
+	},
+}
+
+func init() {
+	cancelCmd.AddCommand(cancelContracts)
+
+}
