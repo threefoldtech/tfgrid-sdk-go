17f296f1a06d06f7226f21a5813c05a5dc41c466
Eslam-Nawara <eslamnawara44@gmail.com>
2024-01-10 17:18:03 +0200

    fix lint

--- mass-deployer/internal/parser/structs.go
+++ mass-deployer/internal/parser/structs.go
@@ -24,18 +24,18 @@ type NodesGroup struct {
 }
 
 type Vm struct {
-	Name        string `yaml:"name"`
-	Count       int    `yaml:"vms_count"`
-	Nodegroup   string `yaml:"node_group"`
-	FreeCPU     int    `yaml:"cpu"`
-	FreeMRU     int    `yaml:"mem"`
-	SSHDisks    []Disk `yaml:"ssd"`
-	Pubip4      bool   `yaml:"pubip4"`
-	Pubip6      bool   `yaml:"pubip6"`
-	Flist       string `yaml:"flist"`
-	Rootsize    int    `yaml:"root_size"`
-	Entrypoint  string `yaml:"entry_point"`
-	SSHKey      string `yaml:"ssh_key"`
+	Name       string `yaml:"name"`
+	Count      int    `yaml:"vms_count"`
+	Nodegroup  string `yaml:"node_group"`
+	FreeCPU    int    `yaml:"cpu"`
+	FreeMRU    int    `yaml:"mem"`
+	SSHDisks   []Disk `yaml:"ssd"`
+	Pubip4     bool   `yaml:"pubip4"`
+	Pubip6     bool   `yaml:"pubip6"`
+	Flist      string `yaml:"flist"`
+	Rootsize   int    `yaml:"root_size"`
+	Entrypoint string `yaml:"entry_point"`
+	SSHKey     string `yaml:"ssh_key"`
 }
 
 type Disk struct {
