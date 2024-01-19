e840c1fe78b9633e7fba708bad07db921050e8ac
mik-tf <logismos@protonmail.ch>
2024-01-19 10:31:30 -0500

    adjusted wallet balance error

--- farmerbot/internal/farmerbot.go
+++ farmerbot/internal/farmerbot.go
@@ -386,7 +386,7 @@ func (f *FarmerBot) validateAccountEnoughBalance(sub *substrate.Substrate, requi
 	}
 
 	if balance.Free.Cmp(big.NewInt(requiredBalanceInTFT)) == -1 {
-		return fmt.Errorf("account contains %f tft, min fee is 0.002 tft", float64(balance.Free.Int64())/math.Pow(10, 7))
+		return fmt.Errorf("account contains %f tft, min fee is 0.002 tft, min wallet balance is 100 tft", float64(balance.Free.Int64())/math.Pow(10, 7))
 	}
 
 	return nil
