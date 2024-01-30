1b34a6833e986b7d31e204e675b9f5670bb06115
Eslam-Nawara <eslamnawara44@gmail.com>
2024-01-30 14:47:14 +0200

    add support to env vars

--- mass-deployer/pkg/mass-deployer/types.go
+++ mass-deployer/pkg/mass-deployer/types.go
@@ -15,8 +15,8 @@ type Config struct {
 type NodesGroup struct {
 	Name       string `yaml:"name" validate:"nonzero" json:"name"`
 	NodesCount uint64 `yaml:"nodes_count" validate:"nonzero" json:"nodes_count"`
-	FreeCPU    uint64 `yaml:"free_cpu" validate:"nonzero" json:"free_cpu"`
-	FreeMRU    uint64 `yaml:"free_mru" validate:"nonzero" json:"free_mru"`
+	FreeCPU    uint64 `yaml:"free_cpu" validate:"nonzero,max=32" json:"cpu"`
+	FreeMRU    uint64 `yaml:"free_mru" validate:"nonzero,min=256,max=262144" json:"mem"` // min: 256MB, max: 256 GB
 	FreeSRU    uint64 `yaml:"free_ssd" json:"free_ssd"`
 	FreeHRU    uint64 `yaml:"free_hdd" json:"free_hdd"`
 	Dedicated  bool   `yaml:"dedicated" json:"dedicated"`
@@ -27,19 +27,20 @@ type NodesGroup struct {
 }
 
 type Vms struct {
-	Name       string `yaml:"name" validate:"nonzero" json:"name"`
-	Count      uint64 `yaml:"vms_count" validate:"nonzero" json:"vms_count"`
-	Nodegroup  string `yaml:"node_group" validate:"nonzero" json:"node_group"`
-	FreeCPU    uint64 `yaml:"cpu" validate:"nonzero,max=32" json:"cpu"`
-	FreeMRU    uint64 `yaml:"mem" validate:"nonzero,min=256,max=262144" json:"mem"` // min: 256MB, max: 256 GB
-	SSDDisks   []Disk `yaml:"ssd" json:"ssd"`
-	PublicIP4  bool   `yaml:"public_ip4" json:"public_ip4"`
-	PublicIP6  bool   `yaml:"public_ip6" json:"public_ip6"`
-	Planetary  bool   `yaml:"planetary" json:"planetary"`
-	Flist      string `yaml:"flist" validate:"nonzero" json:"flist"`
-	Rootsize   uint64 `yaml:"root_size" validate:"max=10240" json:"root_size"` // max 10 TB
-	Entrypoint string `yaml:"entry_point" validate:"nonzero" json:"entry_point"`
-	SSHKey     string `yaml:"ssh_key" validate:"nonzero" json:"ssh_key"`
+	Name       string            `yaml:"name" validate:"nonzero" json:"name"`
+	Count      uint64            `yaml:"vms_count" validate:"nonzero" json:"vms_count"`
+	Nodegroup  string            `yaml:"node_group" validate:"nonzero" json:"node_group"`
+	FreeCPU    uint64            `yaml:"cpu" validate:"nonzero,max=32" json:"cpu"`
+	FreeMRU    uint64            `yaml:"mem" validate:"nonzero,min=256,max=262144" json:"mem"` // min: 256MB, max: 256 GB
+	SSDDisks   []Disk            `yaml:"ssd" json:"ssd"`
+	PublicIP4  bool              `yaml:"public_ip4" json:"public_ip4"`
+	PublicIP6  bool              `yaml:"public_ip6" json:"public_ip6"`
+	Planetary  bool              `yaml:"planetary" json:"planetary"`
+	Flist      string            `yaml:"flist" validate:"nonzero" json:"flist"`
+	Rootsize   uint64            `yaml:"root_size" validate:"max=10240" json:"root_size"` // max 10 TB
+	Entrypoint string            `yaml:"entry_point" validate:"nonzero" json:"entry_point"`
+	SSHKey     string            `yaml:"ssh_key" validate:"nonzero" json:"ssh_key"`
+	EnvVars    map[string]string `yaml:"env_vars" json:"env_vars"`
 }
 
 type Disk struct {
