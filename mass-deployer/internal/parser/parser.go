24bed9777068d74dadabaf4fb9954ab68afad04c
rawdaGastan <rody.rawda.rr@gmail.com>
2024-02-01 15:41:03 +0200

    add ssh key validation in mass deployer

--- mass-deployer/internal/parser/parser.go
+++ mass-deployer/internal/parser/parser.go
@@ -7,6 +7,7 @@ import (
 	"os"
 	"strings"
 
+	"github.com/gliderlabs/ssh"
 	"github.com/go-playground/validator/v10"
 	"github.com/rs/zerolog/log"
 	deployer "github.com/threefoldtech/tfgrid-sdk-go/mass-deployer/pkg/mass-deployer"
@@ -64,6 +65,13 @@ func ParseConfig(file io.Reader, jsonFmt bool) (deployer.Config, error) {
 	for _, nodeGroup := range conf.NodeGroups {
 		name := strings.TrimSpace(nodeGroup.Name)
 		nodeGroupsNames = append(nodeGroupsNames, name)
+	}
+
+	for name, sshKey := range conf.SSHKeys {
+		_, _, _, _, err := ssh.ParseAuthorizedKey([]byte(sshKey))
+		if err != nil {
+			return deployer.Config{}, fmt.Errorf("ssh key for `%s` is invalid: %+w", name, err)
+		}
 	}
 
 	if err := validateVMs(conf.Vms, nodeGroupsNames, conf.SSHKeys); err != nil {
