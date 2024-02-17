8a945cfb318004797214d8909e61c56aecede1e0
Omar Abdulaziz <omarabdul3ziz@gmail.com>
2024-02-17 20:11:28 +0200

    add document and tests: - add comments on the query parser part, documents what each step do and why it is needed. - add tests covers the possible scenarios for the parse methods

--- grid-proxy/internal/explorer/param_parser.go
+++ grid-proxy/internal/explorer/param_parser.go
@@ -17,19 +17,22 @@ func init() {
 	decoder = schema.NewDecoder()
 }
 
-func parseQueryParams(r *http.Request, values ...interface{}) error {
-
+func parseQueryParams(r *http.Request, types_ ...interface{}) error {
 	params := ignoreEmptyParams(r.URL.Query())
 
 	limitParams := make(map[string][]string)
 	filterParams := make(map[string][]string)
 
+	// a helper step to be able to decide if a param is limit query or a filter
 	limitKeys := make(map[string]struct{})
 	for _, key := range getSchemaTags(types.Limit{}) {
 		limitKeys[key] = struct{}{}
 	}
 
-	for _, value := range values {
+	for _, type_ := range types_ {
+		// separate the values of filter/limit into two maps so it is clear what we decode in this iteration
+		// not doing this will make the decoding always fails cause r.URL.Query slice will not fit in only filter or limit
+		// but it has values for both
 		for key, val := range params {
 			if _, ok := limitKeys[key]; ok {
 				limitParams[key] = val
@@ -38,12 +41,16 @@ func parseQueryParams(r *http.Request, values ...interface{}) error {
 			}
 		}
 
+		// deciding which param values will be decoded in the iteration
+		// default it will be the filter map (for nodes/farms/etc..)
+		// but if the interface is inferable to `Limit` type then it is limit
 		param := filterParams
-		if _, ok := value.(*types.Limit); ok {
+		if _, ok := type_.(*types.Limit); ok {
 			param = limitParams
 		}
 
-		if err := decoder.Decode(value, param); err != nil {
+		// the decoder will decode the map of values from param variable to the related passed object
+		if err := decoder.Decode(type_, param); err != nil {
 			return fmt.Errorf("failed to decode params: %w", err)
 		}
 	}
@@ -51,6 +58,7 @@ func parseQueryParams(r *http.Request, values ...interface{}) error {
 	return nil
 }
 
+// returns a slice of all the possible params for this type
 func getSchemaTags(type_ interface{}) []string {
 	t := reflect.TypeOf(type_)
 	if t.Kind() == reflect.Ptr {
@@ -70,6 +78,7 @@ func getSchemaTags(type_ interface{}) []string {
 	return tags
 }
 
+// returns a map of params that have a value
 func ignoreEmptyParams(params url.Values) url.Values {
 	newParams := make(url.Values)
 	for key, val := range params {
