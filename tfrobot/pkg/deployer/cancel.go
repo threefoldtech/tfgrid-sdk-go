23a72c3fbc3e8914c7db70a94d650cecea06dadb
Eslam-Nawara <eslamnawara44@gmail.com>
2024-02-20 15:29:15 +0200

    fix cancel command

--- tfrobot/pkg/deployer/cancel.go
+++ tfrobot/pkg/deployer/cancel.go
@@ -8,16 +8,20 @@ import (
 
 func RunCanceler(cfg Config, tfPluginClient deployer.TFPluginClient, debug bool) error {
 	for _, group := range cfg.NodeGroups {
-		// try to delete group with the new name format "vm/<group name>"
+
+		// try to delete group twice with the new and old name formats
+		// tfrobot shouldn't create two projects with both new and old name format
+		// so only one of the two `CancelByProjectName` would have effect
+
 		name := fmt.Sprintf("vm/%s", group.Name)
 		err := tfPluginClient.CancelByProjectName(name, true)
 		if err != nil {
-			fmt.Println("here??")
-			// if cancelation failed try to delete group with the old name format "<group name>"
-			err := tfPluginClient.CancelByProjectName(group.Name, true)
-			if err != nil {
-				return err
-			}
+			return err
+		}
+
+		err = tfPluginClient.CancelByProjectName(group.Name, true)
+		if err != nil {
+			return err
 		}
 	}
 	return nil
