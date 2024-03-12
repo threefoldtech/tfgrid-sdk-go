15f0797a32432750f1767191afb7b47ee45ce842
Eslam-Nawara <eslamnawara44@gmail.com>
2024-03-12 11:47:42 +0200

    use default proxy url and remove ninja

--- tfrobot/cmd/setup.go
+++ tfrobot/cmd/setup.go
@@ -1,9 +1,6 @@
 package cmd
 
 import (
-	"os"
-	"strings"
-
 	"github.com/rs/zerolog/log"
 	"github.com/threefoldtech/tfgrid-sdk-go/grid-client/deployer"
 	tfrobot "github.com/threefoldtech/tfgrid-sdk-go/tfrobot/pkg/deployer"
@@ -16,18 +13,10 @@ func setup(conf tfrobot.Config, debug bool) (deployer.TFPluginClient, error) {
 	mnemonic := conf.Mnemonic
 	log.Debug().Str("mnemonic", mnemonic).Send()
 
-	var proxyURL string
-	noNinjaProxyURL := strings.TrimSpace(strings.ToLower(os.Getenv("NO_NINJA_PROXY_URL")))
-
-	if network == "main" && noNinjaProxyURL == "" {
-		proxyURL = "https://gridproxy.bknd1.ninja.tf"
-	}
-
 	opts := []deployer.PluginOpt{
 		deployer.WithTwinCache(),
 		deployer.WithRMBTimeout(30),
 		deployer.WithNetwork(network),
-		deployer.WithProxyURL(proxyURL),
 	}
 	if debug {
 		opts = append(opts, deployer.WithLogs())
