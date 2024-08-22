a75e06a8aa88712ae394d459fc32153950f8872c
nabil salah <nabil.salah203@gmail.com>
2024-08-22 13:26:54 +0300

    fix: user not sending nodes issue
    Signed-off-by: nabil salah <nabil.salah203@gmail.com>

--- grid-client/deployer/network_deployer.go
+++ grid-client/deployer/network_deployer.go
@@ -50,11 +50,6 @@ func (d *NetworkDeployer) Validate(ctx context.Context, znets []*workloads.ZNet)
 			multiErr = multierror.Append(multiErr, err)
 			//continue
 		}
-		if len(znet.Nodes) == 0 {
-			err := errors.Errorf("no. of nodes in znet: %s should be nonzero positive number", znet.Name)
-			multiErr = multierror.Append(multiErr, err)
-			continue
-		}
 		if err := znet.Validate(); err != nil {
 			multiErr = multierror.Append(multiErr, err)
 			continue
