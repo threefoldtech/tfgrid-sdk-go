d97ba06f6c189085354adf5cfe38e0dc780fbe86
Omar Abdulaziz <omarabdul3ziz@gmail.com>
2024-10-20 16:56:02 +0300

    update node features set in validators

--- grid-proxy/pkg/types/validators.go
+++ grid-proxy/pkg/types/validators.go
@@ -7,11 +7,12 @@ import (
 
 var (
 	Zos3NodesFeatures = []string{"zmount", "zdb", "volume", "ipv4", "ip", "gateway-name-proxy",
-		"gateway-fqdn-proxy", "qsfs", "zlogs", "network", "zmachine"}
-	Zos4NodesFeatures = []string{"zmount", "zdb", "volume", "ipv4", "ip", "gateway-name-proxy",
-		"gateway-fqdn-proxy", "qsfs", "zlogs", "zmachine-light", "network-light"}
+		"gateway-fqdn-proxy", "qsfs", "zlogs", "network", "zmachine", "wireguard", "yggdrasil", "mycelium"}
+	Zos4NodesFeatures = []string{"zmount", "zdb", "volume", "gateway-name-proxy",
+		"gateway-fqdn-proxy", "qsfs", "zlogs", "zmachine-light", "network-light", "mycelium"}
 	FeaturesSet = []string{"zmount", "zdb", "volume", "ipv4", "ip", "gateway-name-proxy",
-		"gateway-fqdn-proxy", "qsfs", "zlogs", "network", "zmachine", "zmachine-light", "network-light"}
+		"gateway-fqdn-proxy", "qsfs", "zlogs", "network", "zmachine", "zmachine-light", "network-light",
+		"wireguard", "yggdrasil", "mycelium"}
 )
 
 func validateNodeFeatures(features []string) error {
