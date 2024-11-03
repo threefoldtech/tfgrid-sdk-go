ce7c5a31b69c91053a99ba8ecd9feeebc69bdf5c
Eslam-Nawara <eslamnawara44@gmail.com>
2024-11-03 17:09:33 +0200

    add with no clolor logs option to the tfplugin

--- grid-client/deployer/tf_plugin_client.go
+++ grid-client/deployer/tf_plugin_client.go
@@ -79,6 +79,7 @@ type pluginCfg struct {
 	graphqlURLs   []string
 	rmbTimeout    int
 	showLogs      bool
+	noColorLogs   bool
 	rmbInMemCache bool
 }
 
@@ -123,6 +124,12 @@ func WithRMBTimeout(rmbTimeout int) PluginOpt {
 func WithLogs() PluginOpt {
 	return func(p *pluginCfg) {
 		p.showLogs = true
+	}
+}
+
+func WithNoColorLogs() PluginOpt {
+	return func(p *pluginCfg) {
+		p.noColorLogs = true
 	}
 }
 
@@ -208,7 +215,7 @@ func NewTFPluginClient(
 		return TFPluginClient{}, err
 	}
 
-	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
+	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr, NoColor: cfg.noColorLogs})
 	if cfg.showLogs {
 		zerolog.SetGlobalLevel(zerolog.DebugLevel)
 	} else {
