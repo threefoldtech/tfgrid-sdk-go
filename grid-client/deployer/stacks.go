3d4ed447379e46325904ef0c3ec68d3452bbd11d
Omar Abdulaziz <omarabdul3ziz@gmail.com>
2025-07-09 10:24:26 +0300

    update network URLs to use threefold.me domains

--- grid-client/deployer/stacks.go
+++ grid-client/deployer/stacks.go
@@ -11,26 +11,26 @@ var (
 		DevNetwork: {
 			"wss://tfchain.dev.grid.tf/ws",
 			"wss://tfchain.dev.grid.tf:443",
-			"wss://tfchain.02.dev.grid.tf/ws",
-			"wss://tfchain.02.dev.grid.tf:443",
+			"wss://tfchain.dev.threefold.me/ws",
+			"wss://tfchain.dev.threefold.me:443",
 		},
 		QaNetwork: {
 			"wss://tfchain.qa.grid.tf/ws",
 			"wss://tfchain.qa.grid.tf:443",
-			"wss://tfchain.02.qa.grid.tf/ws",
-			"wss://tfchain.02.qa.grid.tf:443",
+			"wss://tfchain.qa.threefold.me/ws",
+			"wss://tfchain.qa.threefold.me:443",
 		},
 		TestNetwork: {
 			"wss://tfchain.test.grid.tf/ws",
 			"wss://tfchain.test.grid.tf:443",
-			"wss://tfchain.02.test.grid.tf/ws",
-			"wss://tfchain.02.test.grid.tf:443",
+			"wss://tfchain.test.threefold.me/ws",
+			"wss://tfchain.test.threefold.me:443",
 		},
 		MainNetwork: {
 			"wss://tfchain.grid.tf/ws",
 			"wss://tfchain.grid.tf:443",
-			"wss://tfchain.02.grid.tf/ws",
-			"wss://tfchain.02.grid.tf:443",
+			"wss://tfchain.threefold.me/ws",
+			"wss://tfchain.threefold.me:443",
 		},
 	}
 
@@ -38,19 +38,19 @@ var (
 	ProxyURLs = map[string][]string{
 		DevNetwork: {
 			"https://gridproxy.dev.grid.tf/",
-			"https://gridproxy.02.dev.grid.tf/",
+			"https://gridproxy.dev.threefold.me/",
 		},
 		TestNetwork: {
 			"https://gridproxy.test.grid.tf/",
-			"https://gridproxy.02.test.grid.tf/",
+			"https://gridproxy.test.threefold.me/",
 		},
 		QaNetwork: {
 			"https://gridproxy.qa.grid.tf/",
-			"https://gridproxy.02.qa.grid.tf/",
+			"https://gridproxy.qa.threefold.me/",
 		},
 		MainNetwork: {
 			"https://gridproxy.grid.tf/",
-			"https://gridproxy.02.grid.tf/",
+			"https://gridproxy.threefold.me/",
 		},
 	}
 
@@ -58,19 +58,19 @@ var (
 	GraphQlURLs = map[string][]string{
 		DevNetwork: {
 			"https://graphql.dev.grid.tf/graphql",
-			"https://graphql.02.dev.grid.tf/graphql",
+			"https://graphql.dev.threefold.me/graphql",
 		},
 		TestNetwork: {
 			"https://graphql.test.grid.tf/graphql",
-			"https://graphql.02.test.grid.tf/graphql",
+			"https://graphql.test.threefold.me/graphql",
 		},
 		QaNetwork: {
 			"https://graphql.qa.grid.tf/graphql",
-			"https://graphql.02.qa.grid.tf/graphql",
+			"https://graphql.qa.threefold.me/graphql",
 		},
 		MainNetwork: {
 			"https://graphql.grid.tf/graphql",
-			"https://graphql.02.grid.tf/graphql",
+			"https://graphql.threefold.me/graphql",
 		},
 	}
 
@@ -78,19 +78,19 @@ var (
 	RelayURLs = map[string][]string{
 		DevNetwork: {
 			"wss://relay.dev.grid.tf",
-			"wss://relay.02.dev.grid.tf",
+			"wss://relay.dev.threefold.me",
 		},
 		TestNetwork: {
 			"wss://relay.test.grid.tf",
-			"wss://relay.02.test.grid.tf",
+			"wss://relay.test.threefold.me",
 		},
 		QaNetwork: {
 			"wss://relay.qa.grid.tf",
-			"wss://relay.02.qa.grid.tf",
+			"wss://relay.qa.threefold.me",
 		},
 		MainNetwork: {
 			"wss://relay.grid.tf",
-			// "wss://relay.02.grid.tf",
+			"wss://relay.threefold.me",
 		},
 	}
 
